0c59e246f2bb71422356952239aa0a96d5b2eba4
f4anks <frankhsistemas@gmail.com>
2026-02-09 11:57:50 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,43 @@
-const CACHE_NAME = 'control-voley-v1';
+const CACHE_NAME = 'voley-stats-v1';
+// Lista de archivos para guardar permanentemente en el celular
 const ASSETS = [
   './',
   './index.html',
-  './index_menu.html',
-  './index_atletas.html',
-  './index_datos.html',
-  './index_ficha.html',
-  './index_cumple.html',
-  './index_estad.html',
-  './img/banner.png'
+  './manifest.json',
+  './libs/tailwind.js',
+  './libs/charts.js',
+  './img/logo.png'
 ];
 
-// Instalación: Guardar archivos en caché
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+// 1. Instalación: Guarda los archivos en el disco del celular
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Cacheando archivos para modo offline...');
       return cache.addAll(ASSETS);
     })
   );
+  // Fuerza a que el SW se active de inmediato
+  self.skipWaiting();
 });
 
-// Estrategia de carga: Primero caché, luego red
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
+// 2. Activación: Limpia caches viejas
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// 3. Intercepción: Si no hay internet, sirve los archivos del cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
