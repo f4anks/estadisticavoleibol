a452a7c9f7ffecd894bec4e0fa80ea140b45868d
f4anks <frankhsistemas@gmail.com>
2026-02-09 12:59:19 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'cv-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './libs/tailwind.js',
+  './libs/charts.js',
+  './img/logo.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
